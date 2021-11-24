3195ce110e8b9f7e7cb6aeb83cad3acefbb9f38a
soottoppi <duwjs1103@naver.com>
2021-11-24 12:24:39 +0900

    jsx tutorials

---
+++ 01.basics/ex08/src/02/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function(){
+    /*
+        리액트 컴포넌트는 단일 루트 노트만 랜더링할 수 있다
+        오류
+        <h2>App02</h2>
+        <p>JSX Tutorials - 특징2: Single Root node</p>
+    */
+    return (
+        <div>
+            <h2>App02</h2>
+            <p>JSX Tutorials - 특징2: Single Root node</p>
+        </div>
+    )
+};
