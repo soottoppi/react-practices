9c7e72a7535a03e73a9d799f4cd8331db66c9558
soottoppi <duwjs1103@naver.com>
2021-11-25 11:54:48 +0900

    jsx tutorials #Comments

---
+++ 01.basics/ex08/src/08/Content.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react';
+import Clock from './Clock'
+
+export default function() {
+    return (
+        <Fragment>
+            <p>Comment</p>
+            <Clock />
+        </Fragment>
+        
+    );
+}
+
---
+++ 01.basics/ex08/src/08/Header.js
@@ -0,0 +1,7 @@
+import React, {Fragment} from 'react';
+
+export default function Header() {
+    return (
+        <h2>App08</h2>  
+    );
+}
