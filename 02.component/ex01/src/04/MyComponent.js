ff3b90b0e361a8dfd95a6f349d6778c9f0b2ff42
soottoppi <duwjs1103@naver.com>
2021-11-29 10:22:28 +0900

    Kanban #CardList, Card

--- 02.component/ex01/src/04/MyComponent.js
+++ 02.component/ex01/src/04/MyComponent.js
@@ -2,7 +2,7 @@ import React, {Fragment} from 'react';
 import {PropTypes} from 'prop-types';
 
 
-export default function MyComponent({props01, props02, props03, props04, props05, props06}) {
+export default function MyComponent({props01, props02, props03, props04, props05, props06, props07, props08, props09}) {
     return (
         <Fragment >
             <h2>Property Validation</h2>
@@ -25,6 +25,22 @@ export default function MyComponent({props01, props02, props03, props04, props05
             <span>props06: { props06 ? props06() : '--- Not Set ---' }</span>
             <br/>
 
+            <span>props07: { props07 ? props07 : '--- Not Set ---' }</span>
+            <br/>
+
+            <span>props08: { props08 ? props08.map((e, i) => e ? <b key={i}>{'true'}</b> : <b key={i}>{'false'}</b> ) : '--- Not Set ---'}</span>
+            <br/>
+
+            <span>
+                props09: { 
+                    props09 ?
+                        <div>
+                            <h3>{props09.no}</h3>
+                            <h4>{props09.name}</h4>
+                            <h5>{props09.email}</h5>
+                        </div> :
+                        '--- Not Set ---'
+            }</span>
         </Fragment>
     )
 
@@ -37,7 +53,16 @@ MyComponent.propTypes = {
     props03: PropTypes.bool.isRequired,
     props04: PropTypes.object.isRequired,
     props05: PropTypes.array.isRequired,
-    props06: PropTypes.func.isRequired
+    props06: PropTypes.func.isRequired,
+
+    // Built-In PropTypes Validator(Combined Primitive)
+    props07: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+    props08: PropTypes.arrayOf(PropTypes.bool).isRequired,
+    props09: PropTypes.shape({
+        no: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired,
+        email: PropTypes.string.isRequired,
+    }).isRequired,
 }
 
 // default value
---
+++ 02.component/kanbanboard/src/01/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function({title, description, tasks}) {
+    return (
+        <div className='Card'>
+            <div className='Card__Title'>{title}</div>
+            <div className='Card__Details'>
+                {description}
+            </div>
+            <taskList tasks={tasks} />
+            <div className='TaskList'>
+                <ul>
+                </ul>
+            </div>
+        </div>
+    )
+}
